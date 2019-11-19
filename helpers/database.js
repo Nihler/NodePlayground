21b4fc12c00393772242190c9ae0142e397ad0df
Nihler <vanilor97@gmail.com>
2019-11-20 00:48:52 +0100

    test new db

--- helpers/database.js
+++ helpers/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("internety", "root", "admin", {
-  dialect: "mysql",
-  host: "localhost"
-});
+const sequelize = new Sequelize(
+  "db37h7q596s9q3",
+  "poqejnkidehtkl",
+  "b1313bf7ef18c8359ac1713a32f805f078787bd385f999c996195e89743958be",
+  {
+    dialect: "postgres",
+    host: "ec2-54-228-243-238.eu-west-1.compute.amazonaws.com"
+  }
+);
 
 module.exports = sequelize;
